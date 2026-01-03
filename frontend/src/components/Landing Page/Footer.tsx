420172ba0897ff2596dc6f545ce090325696900a
garimakumarix29 <garimakumari.x29@gmail.com>
2026-01-03 11:08:25 +0530

    footer UI update

--- frontend/src/components/Landing Page/Footer.tsx
+++ frontend/src/components/Landing Page/Footer.tsx
@@ -9,16 +9,30 @@ interface Props {
 export default function Footer({ activeSection }: Props) {
   const footerStyle: React.CSSProperties = {
     display: "grid",
-    gridTemplateColumns: "1fr 2fr 1fr",
-    padding: "80px 100px",
+    gridTemplateColumns: "1fr 2fr 1.5fr",
+    padding: "30px 50px",
     background: "black",
     borderTop: "1px solid rgba(255,255,255,0.08)",
     color: "white",
+    alignItems: "start",
   };
 
-  const logoStyle: React.CSSProperties = { fontSize: 24, fontWeight: 600 };
-  const linksStyle: React.CSSProperties = { display: "flex", gap: "80px" };
-  const h4Style: React.CSSProperties = { marginBottom: 16, fontSize: 14, opacity: 0.8 };
+  const logoStyle: React.CSSProperties = {
+    fontSize: 24,
+    fontWeight: 600,
+  };
+
+  const linksStyle: React.CSSProperties = {
+    display: "flex",
+    gap: "150px",
+  };
+
+  const h4Style: React.CSSProperties = {
+    marginBottom: 16,
+    fontSize: 16,
+    opacity: 0.8,
+  };
+
   const linkStyle: React.CSSProperties = {
     display: "block",
     marginBottom: 10,
@@ -26,14 +40,22 @@ export default function Footer({ activeSection }: Props) {
     opacity: 0.7,
     color: "inherit",
     textDecoration: "none",
+    cursor: "pointer",
   };
-  const visualStyle: React.CSSProperties = { display: "flex", justifyContent: "flex-end", alignItems: "center" };
-  const nowStyle: React.CSSProperties = { textTransform: "capitalize", opacity: 0.7 };
+
+  const visualStyle: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    position: "relative",
+  };
 
   return (
     <footer style={footerStyle} data-active={activeSection ?? "hero"}>
+      {/* Logo */}
       <div style={logoStyle}>K-Folio</div>
 
+      {/* Links */}
       <div style={linksStyle}>
         <div>
           <h4 style={h4Style}>Explore</h4>
@@ -53,9 +75,19 @@ export default function Footer({ activeSection }: Props) {
         </div>
       </div>
 
+      {/* Visual / Illustration */}
       <div style={visualStyle}>
-        <div style={nowStyle}>{activeSection}</div>
+        <img
+          src="/footer.svg"
+          alt="Footer illustration"
+          style={{
+            width: "500px",
+            maxWidth: "100%",
+            height: "auto",
+          }}
+        />
       </div>
     </footer>
   );
 }
+
